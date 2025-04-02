a90832e055014c28481dd99fa1eb4a8d657e64ec
Reza Mesgaran <Reza.Mesgaran@staples.ca>
2025-04-02 10:37:43 -0400

    Add Readme.md

--- Console_Casting-Implicit-Explicit/Program.cs
+++ Console_Casting-Implicit-Explicit/Program.cs
@@ -1,31 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-/*
-Note:
-    A.Implicit conversions:
-        1.No syntax required 
-        2.type safe conversions (No data will be lost)
-        3.Example : 
-            a.conversions from smaller to larger integral types :(int -> double)
-            b.Conversions from derived classes to base classe 
-
-
-    B.Explicit conversions (Cast):
-        1.Syntax required 
-        2.NOT type safe conversions (data loss might occur)
-        3.Example : 
-            a.conversions from larger to smaller integral types :(Double -> int)
-            b.Conversions from base classe to derived classes
-
-    C.Parse / TryParse
-    
-    D.Convert.To_X
-
-*/
 namespace Console_Casting_Implicit_Explicit
 {
     class ClassBase { }
@@ -40,18 +14,18 @@ namespace Console_Casting_Implicit_Explicit
             double y = x;
 
             // A2.Implicit conversions  (DrivedClass -> BaseClass)
-            ClassDerived D = new ClassDerived();
-            ClassBase B = D;
+            ClassDerived derivedClass = new ClassDerived();
+            ClassBase baseClass = derivedClass;
 
             //===================================================================
 
             // B1.Explicit conversions (Larger -> Smaller)
-            double d = 123.4567;
-            int nd = (int)d;   // Output: 123 
+            double doubleValue = 123.4567;
+            int intValue = (int)doubleValue;   // Output: 123 
 
             // B2.Explicit conversions   (BaseClass -> DrivedClass)
-            ClassBase Ba = new ClassBase();
-            ClassDerived De= (ClassDerived)Ba;
+            ClassBase baseClass2 = new ClassDerived();
+            ClassDerived deliveredClass2 = (ClassDerived)baseClass2;
 
             //===================================================================
 
@@ -60,8 +34,10 @@ namespace Console_Casting_Implicit_Explicit
             string strName = "Reza";
             string strId = "0010";
 
-            bool Result = int.TryParse(strName, out int converted_strName);  //bool:TryParse(string,Int) : Check possibility of converting digits-OnString -> int and retrun boolean
-            int converted_strId = int.Parse(strId); // int.Parse(string) : Convert String -> int
+            bool Result = int.TryParse(strName, out int converted_strName);
+            //bool:int.TryParse(string,out Int) : Check possibility of converting digits-OnString -> int and retruns a Boolean result + int of converted string!
+
+            int converted_strId = int.Parse(strId); // int.Parse(string) : Convert String -> int and returns int value!
 
             //===================================================================
 
